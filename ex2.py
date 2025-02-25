f98da43e8894408a5655670381d97d614b0d9727
filipe-eich <filipe.eich@gmail.com>
2025-02-25 20:40:33 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,27 @@
+"""
+Programa: soma_2_3
+Descrição: Soma 2 e 3
+Autor: Filipe Eich
+Data: 25/02/2025
+Versao: 0.0.1
+
+"""
+
+
+#Alocaçao de memoria
+
+var1=2
+var2=3
+
+
+
+# Processamento de dados
+
+soma=var1+var2
+
+#Saida de dados
+
+
+print(soma)
+
+
